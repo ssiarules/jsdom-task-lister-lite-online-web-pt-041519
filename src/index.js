f9a4d951653af7c554a9add5f6b621e589bf9cd1
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-07 17:37:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+    //set variables for the form, input, and ul
+    let form = document.querySelector('form')
+    let input = document.querySelector('input#new-task-description')
+    let ul = document.querySelector('ul#tasks')
+
+  
 });
