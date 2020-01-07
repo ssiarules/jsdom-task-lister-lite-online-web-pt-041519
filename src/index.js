6ed881845c0c28ec4562fe255a8dd9b61f62e9df
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-07 17:41:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,5 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
     let input = document.querySelector('input#new-task-description')
     let ul = document.querySelector('ul#tasks')
 
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      //create new task li, set to input value, and append to the ul
+      let newTask = document.createElement('li')
+      newTask.innerText = input.value
+      ul.appendChild(newTask)
+      
+    })
+
 
 });
