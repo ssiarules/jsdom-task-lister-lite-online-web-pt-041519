bb6fb7ef3bf23d9e33b243ee818fb8b6bc2e6cf5
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-07 18:13:26 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+    //set variables for the form, input, and ul
+    let form = document.querySelector('form')
+    let input = document.querySelector('input#new-task-description')
+    let ul = document.querySelector('ul#tasks')
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      //create new task li, set to input value, and append to the ul
+      let newTask = document.createElement('li')
+      newTask.innerText = input.value
+      ul.appendChild(newTask)
+
+      //Create and set up delete button
+      let deleteButton = document.createElement('button')
+      deleteButton.innerText = 'X'
+      newTask.appendChild(deleteButton)
+
+      //Add listner to delete li on button click
+      deleteButton.addEventListener('click', (e) => e.target.parentNode.remove())
+
+      //clear the input field
+      input.value = ''
+
+    })
 });
